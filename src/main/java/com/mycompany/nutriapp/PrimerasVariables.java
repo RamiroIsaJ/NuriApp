bd351345a17841a2c4cb0ebba7914f420b4d8c70
Sebastián <sebastian.aucapinia@espoch.edu.ec>
2023-06-25 10:18:32 -0500

    Prueba Cambio

--- src/main/java/com/mycompany/nutriapp/PrimerasVariables.java
+++ src/main/java/com/mycompany/nutriapp/PrimerasVariables.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.mycompany.nutriapp;
-
+//Librerias
 import Clases.CConexion;
 import java.awt.Color;
 import java.sql.CallableStatement;
